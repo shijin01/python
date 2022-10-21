db30bb0afbbef1cc8f73cda52cf5daa5bc833c07
shijin01 <63602111+shijin01@users.noreply.github.com>
2022-10-21 10:19:53 +0530

    Add files via upload

---
+++ deg2fahrenheit.py
@@ -0,0 +1,3 @@
+d=float(input("Enter the temperature in degree celsius:"))
+f=d*9/5+32
+print("Temperature in fahrenheit:",f)
---
+++ swapwithout_t.py
@@ -0,0 +1,14 @@
+'''a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+print("Before swapping\n a:",a,"\n b:",b)
+a+=b
+b=a-b
+a-=b
+print("After swapping\n a:",a,"\n b:",b)'''
+
+c=int(input("Enter number1:"))
+d=int(input("Enter number2:"))
+print("Before swapping\n a:",c,"\n b:",d)
+c,d=d,c
+print("After swapping\n a:",c,"\n b:",d)
+
