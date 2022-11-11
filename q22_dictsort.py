061570e28e37f8edc327d54681ed59a3c0b37351
shijin01 <63602111+shijin01@users.noreply.github.com>
2022-11-11 11:49:51 +0530

    Add files via upload

--- q22_dictsort.py
+++ q22_dictsort.py
@@ -10,4 +10,8 @@ d_dict=sorted(d1.items(),reverse=1)
 
 print("\nAscending Order : ",dict(a_dict))
 print("\nDescending Order : ",dict(d_dict))
+a_dict=sorted(d1.items(),key=lambda item:item[1],reverse=0)
+d_dict=sorted(d1.items(),key=lambda item:item[1],reverse=1)
+print("\nSort by Value:\nAscending Order : ",dict(a_dict))
+print("\nDescending Order : ",dict(d_dict))
 
