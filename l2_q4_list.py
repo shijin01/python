606e2dd7df8ff4ff7ba66352544f67af86482436
shijin01 <63602111+shijin01@users.noreply.github.com>
2022-11-18 15:44:25 +0530

    Add files via upload

---
+++ l2_q4_list.py
@@ -0,0 +1,22 @@
+
+def createlist(a,b):
+	l=[]
+	s=[x*x for x in range(32,100)]
+	for i in range(a+1,b):
+		if isdigiteven(i)==1 and i in s:
+			l.append(i)
+			
+	print(f"List of four digit numbers in a {a} and {b} with all their digits even and the number is a perfect square:{l}")		
+			
+def isdigiteven(n):
+	while(n>0):
+		d=n%10
+		if(d%2!=0):
+			return 0
+		n=int(n/10)
+	return 1
+	
+a=int(input("Enter the range:"))
+b=int(input())
+createlist(a,b)
+
